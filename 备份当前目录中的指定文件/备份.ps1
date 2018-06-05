80dbd48c35c7d511a00bc7d8a5bac0b00483874c
肖传令 <xiaochuanling@91bihu.com>
2018-06-05 11:11:06 +0800

    提交
    Signed-off-by: 肖传令 <xiaochuanling@91bihu.com>

---
+++ 备份当前目录中的指定文件/备份.ps1
@@ -0,0 +1,15 @@
+﻿
+$time = Get-Date -Format 'yyyyMMdd HHmmss'
+echo $time
+if(Test-Path .\$time){del -Recurse .\$time}
+mkdir $time
+
+foreach($file in dir .)
+{
+        #echo $file.Name  # 这里可以显示文件名，
+      
+   
+        if((Get-Item $file) -is [IO.DirectoryInfo]){continue}
+        if($file.Name -eq "备份.ps1"){continue}
+        copy $file .\$time
+}
